5d0098c65c5408fdad3d7f144666518ea30c7685
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-03-05 15:12:53 +1100

    Added setup method to index.rb

--- src/index.rb
+++ src/index.rb
@@ -1,3 +1,4 @@
+require 'faker'
 
 secret_word = nil
 attempts_left = nil
@@ -39,7 +40,28 @@ def welcome
   if username.strip == "" then username = "Player" end
 end
 
-#setup
+def setup
+    # attempts allowed before game over
+    attempts_left = nil
+    # empty array to store guesses
+    letters_used = []
+    # secret_word letters replaced by "_"
+    progress = []
+    
+    # secret_word = “random_word_from_faker_gem”
+    secret_word = Faker::Address.country
+
+    # secret_word letters replaced by "_"
+    secret_word.each_char {|c| 
+        if c != " " then 
+            progress.push("_")
+        else
+            progress.push(" ") 
+        end
+    }
+
+    p progress.join(" ")
+end
 
 def get_user_guess progress
   puts "Guess a letter: "
